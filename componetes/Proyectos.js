53a6f0f461c68d01978116b78e8142b3f01d6a70
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-10-09 18:01:49 -0300

    first proyect styles

---
+++ componetes/Proyectos.js
@@ -0,0 +1,11 @@
+import style from "../styles/Proyectos.module.css"
+
+const Proyectos = () => {
+  return (
+    <div className={style.container}>
+        <h1>Mis Proyectos</h1>
+    </div>
+  )
+}
+
+export default Proyectos
