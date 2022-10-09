907adecf978d2ede29e2da608a987c93aebc46ee
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-10-09 17:59:34 -0300

    first styles to skill

---
+++ componetes/Skill.js
@@ -0,0 +1,12 @@
+import style from "../styles/Skill.module.css"
+
+const Skill = () => {
+  return (
+    <div className={style.container}>
+        <h1>My Skill</h1>
+        
+    </div>
+  )
+}
+
+export default Skill
