f9f42adab4fb850a0374f9e142de97173cd7a49e
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-10-09 18:00:18 -0300

    first styles to about me

---
+++ componetes/SobreMi.js
@@ -0,0 +1,11 @@
+import style from "../styles/SobreMi.module.css"
+
+const SobreMi = () => {
+  return (
+    <div className={style.container}>
+        <h1>Sobre Mi</h1>
+    </div>
+  )
+}
+
+export default SobreMi
