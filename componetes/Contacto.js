022c2637adc2f6b18b1c1e23e442dfe406946d00
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-10-09 18:01:24 -0300

    first contact styles

---
+++ componetes/Contacto.js
@@ -0,0 +1,11 @@
+import style from "../styles/Contacto.module.css"
+
+const Contacto = () => {
+  return (
+    <div className={style.container}>
+        <h1>Contactame</h1>
+    </div>
+  )
+}
+
+export default Contacto
