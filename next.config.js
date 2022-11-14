eb470f9e143584f85bfb951d813237a2fc2d6ee9
Rodriguez Lorena <loreleyro.89@gmail.com>
2022-11-14 18:39:20 -0300

    probando i18n

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es", "pt"],
+    
+  }
 }
 
+// module.exports = {
+//   i18n: {
+//     defaultLocale: "es",
+//     locales: ["es", "en", "pt"],
+    
+//   },
+// }
 module.exports = nextConfig
