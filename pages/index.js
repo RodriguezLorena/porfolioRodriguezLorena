93d1115a421b0d013aca0ec894d70168628b52e8
Lorena Rodriguez <loreleyro.89@gmail.com>
2022-12-05 16:04:49 -0300

    pase de props

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ import LenguageSelector from "../componetes/LenguageSelector";
 
 
 export default function Home(props) {
-  const { preText, title, text, titleSm, textSm, titleHab}= props;
+  const { preText, title, text, titleSm, textSm, titleHab, titlePro, title1, text1, title2, text2, title3, text3, title4, text4, title5, text5, titleCon, textCon, btnCallToAction ,spamContact}= props;
   return (
     <Layout>
       <PaginaInicial 
@@ -26,8 +26,23 @@ export default function Home(props) {
       textSm={textSm}/>
       <Skill 
       titleHab={titleHab}/>
-      <Proyectos />
-      <Contacto />
+      <Proyectos 
+      titlePro={titlePro}
+      title1={title1}
+      text1={text1}
+      title2={title2}
+      text2={text2}
+      title3={title3}
+      text3={text3}
+      title4={title4}
+      text4={text4}
+      title5={title5}
+      text5={text5} />
+      <Contacto 
+      titleCon={titleCon}
+      textCon={textCon}
+      btnCallToAction={btnCallToAction}
+      spamContact={spamContact}/>
     </Layout>
   );
 }
@@ -45,6 +60,23 @@ export async function getStaticProps({ locale }) {
       textSm: res.default.sobreMi.textSm,
 
       titleHab: res.default.habilidades.titleHab,
+
+      titlePro: res.default.proyectos.titlePro,
+      title1: res.default.proyectos.title1,
+      text1: res.default.proyectos.text1,
+      title2: res.default.proyectos.title2,
+      text2: res.default.proyectos.text2,
+      title3: res.default.proyectos.title3,
+      text3: res.default.proyectos.text3,
+      title4: res.default.proyectos.title4,
+      text4: res.default.proyectos.text4,
+      title5: res.default.proyectos.title5,
+      text5: res.default.proyectos.text5,
+
+      titleCon: res.default.contacto.titleCon,
+      textCon: res.default.contacto.textCon,
+      btnCallToAction: res.default.contacto.btnCallToAction,
+      spamContact: res.default.contacto.spamContact,
     },
   };
 }
